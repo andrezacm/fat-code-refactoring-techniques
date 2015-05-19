00eeb4bc8528a465ea017b0063b4b4e101fefb5b
andrezacm <andreza.cmedeiros@gmail.com>
2015-05-19 14:07:38 -0700

    Split Controllers: created following and followers controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 SampleApp::Application.routes.draw do
   resources :users do
     member do
-      get :following, :followers
+      get :followers, controller: 'followers'
+      get :following, controller: 'following'
     end
   end
   resources :sessions,      only: [:new, :create, :destroy]
